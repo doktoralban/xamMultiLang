23f3fd9a6eadc00dc02525e7b84bdbdf9ed95164
doktor alban <doktoralban@outlook.com>
2020-06-27 13:42:53 +0300

    Initial commit

---
+++ xamMultiLang/xamMultiLang/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using xamMultiLang.Models;
+
+namespace xamMultiLang.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
